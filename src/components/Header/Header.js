ce1f03819f67c5357b38550a63201a6b11f2cc44
Andrew Moeggenberg <andrew.moeggenberg.webdev@gmail.com>
2019-11-04 19:35:31 -0500

    nav update

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -6,14 +6,7 @@ const Header = () => {
     <header className="header">
       <nav className="header__nav">
         <h1 className="header__nav--h1">Face Recog</h1>
-        <ul className="header__nav--list">
-          <li className="header__nav--list-item">
-            <a href="http://localhost:3000/">About</a>
-          </li>
-          <li className="header__nav--list-item">
-            <a href="http://localhost:3000/">Sign Out</a>
-          </li>
-        </ul >
+
       </nav>
     </header>
   );
